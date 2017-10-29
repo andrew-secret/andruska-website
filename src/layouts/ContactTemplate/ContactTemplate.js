e40ee8694ca782b24c1ab396777a7555095c2482
andrew-secret <andre.rusakow@jimdo.com>
2017-10-29 16:55:29 +0100

    feat: implement modal contact

---
+++ src/layouts/ContactTemplate/ContactTemplate.js
@@ -0,0 +1,51 @@
+import React from 'react';;
+import styles from './ContactTemplate.module.sass';
+import classnames from 'classnames';
+import Button from '../../Components/Button/Button';
+import Input from '../../Components/Input/Input';
+
+
+const ContactTemplate = (props) => {
+    const ModalContactClasses = classnames(styles.contactOverlay, {
+        [styles.isActive]: props.active === true,
+      });
+    
+    return(<div className={ModalContactClasses}>
+        <button 
+            type="button" 
+            className={styles.closeButton}
+            onClick={props.toggleClass}>
+            <i className="material-icons">close</i>
+        </button>
+        <div className={styles.left}>
+            <h2 className={styles.headline}>
+                <span>Hallo,</span> ich bin André schreib mir doch einfach.
+            </h2>
+        </div>
+        <div className={styles.right}>
+        <form action="//formspree.io/andre.rusakow@gmail.com" method="POST">
+            <Input
+                label="Name"
+                className={styles.inputName}
+                type="name"
+                required="true"/>
+
+            <Input
+                label="E-Mail"
+                className={styles.inputMail}
+                type="email"
+                required="true"/>
+
+            <Input
+                label="Message"
+                className={styles.inputMessage}
+                type="text"
+                required="true"/>
+
+            <Button type="submit"
+                label="Submit"/>
+        </form>
+        </div>
+    </div>)};
+
+export default ContactTemplate;
