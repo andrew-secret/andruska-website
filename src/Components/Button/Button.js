6306949f2c53029381090c66ed548bbcb1c5a178
andrew-secret <andre.rusakow@jimdo.com>
2017-10-29 16:54:10 +0100

    feat: add reusable input and button components

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classnames from 'classnames';
+import styles from './Button.module.sass';
+
+const Button = (props) => {
+    const buttonClasses = classnames(styles.buttonStyle, props.className);
+    return (
+        <button 
+            className={buttonClasses}
+            type={props.type}
+            onClick={props.onClick}>
+            {props.label}
+            <i className="material-icons">
+                {props.icon}
+            </i>
+        </button>
+    )
+}
+
+export default Button;
+
+Button.defaultProps = {
+    type: 'button',
+    label: 'This is a button',
+}
