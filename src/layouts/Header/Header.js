bcdb5e5c6d92a33aee8d0569404b080ad489d049
andrew-secret <andre.rusakow@jimdo.com>
2017-10-30 10:59:42 +0100

    feat: link logo to index page

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -2,15 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Navigation from '../Navigation/Navigation';
 import styles from './Header.module.sass';
+import Link from "gatsby-link";
 import logo from '../../images/andruska-logo.svg';
 
 console.log(styles);
 
 const Header = () => (
     <div className={styles.header}>
-        <img
-            className={styles.logo} 
-            src={logo} alt="Logo" />
+        <Link to="/">
+            <img
+                className={styles.logo} 
+                src={logo} alt="Logo" />
+        </Link>
         <Navigation />
     </div>
 );
