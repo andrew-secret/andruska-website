c1d77a45e4ac83e5945775ee99874f6008debb2f
andrew-secret <andre.rusakow@jimdo.com>
2018-04-19 09:55:21 +0200

    refactor: auto format code

--- src/layouts/Contact/Contact.js
+++ src/layouts/Contact/Contact.js
@@ -1,40 +1,35 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 import styles from './Contact.module.sass';
 import ContactTemplate from '../ContactTemplate/ContactTemplate';
 import create from '../../images/ic_create_black_24px.svg';
 
 class Counter extends Component {
-    constructor() {
-        super();
-        this.state = { active: false };
-        this.toggleClass = this.toggleClass.bind(this);
-    }
-    toggleClass() {
-        const currentState = this.state.active;
-        this.setState({ active: !currentState });
-    };
+  constructor() {
+    super();
+    this.state = { active: false };
+    this.toggleClass = this.toggleClass.bind(this);
+  }
+  toggleClass() {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+  }
 
-    render() {
-        return (
-            <div>
-                <button
-                    className={styles.contactButton}
-                    onClick={this.toggleClass}>
-                    <i className={styles.createIcon}>
-                        <span className={styles.srOnly}>
-                            Contact
-                        </span>
-                    </i>
-                </button>
-                {this.state.active === true ? (
-                    <ContactTemplate
-                        active={this.state.active}
-                        toggleClass={this.toggleClass}/>
-                ) : (
-                    null
-                )}
-            </div>
-        );
+  render() {
+    return (
+      <div>
+        <button className={styles.contactButton} onClick={this.toggleClass}>
+          <i className={styles.createIcon}>
+            <span className={styles.srOnly}>Contact</span>
+          </i>
+        </button>
+        {this.state.active && (
+          <ContactTemplate
+            active={this.state.active}
+            toggleClass={this.toggleClass}
+          />
+        )}
+      </div>
+    );
   }
 }
 
