a667e3b5729d2dc169121b04d0f50afae859f09f
andrew-secret <andre.rusakow@jimdo.com>
2018-05-25 16:39:13 +0200

    feat: setup jest enzyme and extend test-helpers

--- src/lib/test-helpers.tsx
+++ src/lib/test-helpers.tsx
@@ -1,6 +1,6 @@
 import * as shallowRenderer from 'react-test-renderer/shallow';
 import { shallow, configure, ShallowWrapper } from 'enzyme';
-
+import * as deepRenderer from 'react-test-renderer';
 import * as Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
@@ -19,4 +19,10 @@ export function renderComponentShallow(
 // Renders component shallow with react-test-renderer for snapshot tests
 export function renderComponentShallowForSnapshot(component: TestComponent) {
   return shallowRenderer.createRenderer().render(component);
+}
+
+// Renders component deep without providing application environment
+// Gives you access to full api of test renderer
+export function renderComponent(component: TestComponent) {
+  return deepRenderer.create(component);
 }
